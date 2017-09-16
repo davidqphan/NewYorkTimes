51b496219afdcad170de0df9d5a22d4caed0af3c
David Phan <dphan015@ucr.edu>
2017-09-15 20:25:47 -0700

    implements the ArticlesContract.View interface

--- app/src/main/java/com/codepath/newyorktimes/articles/ArticlesFragment.java
+++ app/src/main/java/com/codepath/newyorktimes/articles/ArticlesFragment.java
@@ -1,10 +1,25 @@
 package com.codepath.newyorktimes.articles;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by dphan on 9/15/17.
  */
 
-public class ArticlesFragment extends Fragment {
+public class ArticlesFragment extends Fragment implements ArticlesContract.View {
+
+    private ArticlesContract.Presenter articlesPresenter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setPresenter(@NonNull ArticlesContract.Presenter presenter) {
+        articlesPresenter = presenter;
+    }
 }
