dc533022d091a9bdf3ad623940e57956b2c3c1ab
David Phan <dphan015@ucr.edu>
2017-09-15 20:24:43 -0700

    defines the connection between the View and Presenter

--- app/src/main/java/com/codepath/newyorktimes/articles/ArticlesContract.java
+++ app/src/main/java/com/codepath/newyorktimes/articles/ArticlesContract.java
@@ -1,8 +1,19 @@
 package com.codepath.newyorktimes.articles;
 
+import com.codepath.newyorktimes.BasePresenter;
+import com.codepath.newyorktimes.BaseView;
+
 /**
  * Created by dphan on 9/15/17.
  */
 
 public interface ArticlesContract {
+
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
 }
