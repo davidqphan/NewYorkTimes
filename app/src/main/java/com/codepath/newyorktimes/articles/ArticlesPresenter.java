d97e3513219f2150c53a8b21396498c763e9cb8d
David Phan <dphan015@ucr.edu>
2017-09-15 20:26:23 -0700

    implements the ArticlesContract.Presenter interface

--- app/src/main/java/com/codepath/newyorktimes/articles/ArticlesPresenter.java
+++ app/src/main/java/com/codepath/newyorktimes/articles/ArticlesPresenter.java
@@ -1,8 +1,15 @@
 package com.codepath.newyorktimes.articles;
 
+import com.codepath.newyorktimes.BasePresenter;
+
 /**
  * Created by dphan on 9/15/17.
  */
 
-public class ArticlesPresenter {
+public class ArticlesPresenter implements ArticlesContract.Presenter {
+
+    @Override
+    public void start() {
+
+    }
 }
