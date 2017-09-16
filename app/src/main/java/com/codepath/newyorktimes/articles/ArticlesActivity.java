db1290f32c593602494fa719c94b87c456ca7e79
David Phan <dphan015@ucr.edu>
2017-09-15 20:25:09 -0700

    creates Fragments and Presenters

--- app/src/main/java/com/codepath/newyorktimes/articles/ArticlesActivity.java
+++ app/src/main/java/com/codepath/newyorktimes/articles/ArticlesActivity.java
@@ -1,8 +1,19 @@
 package com.codepath.newyorktimes.articles;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codepath.newyorktimes.R;
+
 /**
  * Created by dphan on 9/15/17.
  */
 
-public class ArticlesActivity {
+public class ArticlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articles);
+    }
 }
